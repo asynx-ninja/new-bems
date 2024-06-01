0d668afcc18cc9d7e029cdca85b70ade576fa50a
Dwayne <xxstopxx3@gmail.com>
2024-06-01 18:46:15 +0800

    12345

--- src/admin/dashboard/dashboard.jsx
+++ src/admin/dashboard/dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DefaultLayout from '../../layouts/DefaultLayout'
 const Dashboard = () => {
+  const acc_type = "Admin"
     const Layout = () => (
         <div>asdadadada</div>
     )
   return (
-    <DefaultLayout component={<Layout />} />
+    <DefaultLayout component={<Layout />}  acc_type={acc_type}/>
   )
 }
 
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from "react"
-import SidebarItem from "../components/sidebar/SidebarItem"
-import { DataSidebar } from "../data/DataSidebar";
+import React, { useState } from "react";
+import SidebarItem from "../components/sidebar/SidebarItem";
+import { AdminDataSidebar } from "../data/AdminDataSidebar";
+import { StaffDataSidebar } from "../data/StaffDataSidebar";
 import SidebarHeader from "../components/sidebar/SidebarHeader";
 import SidebarAlert from "../components/sidebar/SidebarAlert";
 import SidebarFooter from "../components/sidebar/SidebarFooter";
 import TopHeader from "../components/header-footer/TopHeader";
 import Footer from "../components/header-footer/Footer";
 
-export default function DefaultLayout({ component }) {
-  const [isSideNavOpen, setIsSideNavOpen] = useState(false)
+export default function DefaultLayout({ component, acc_type }) {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
-  return (
+  return (  
     <>
       <div className="">
         <aside
           id="nav-menu-4"
           aria-label="Side navigation"
-          className={`fixed top-0 bottom-0 left-0 z-40 flex w-64 flex-col border-r border-r-slate-200 bg-white transition-transform sm:translate-x-0 ${isSideNavOpen ? "translate-x-0" : " -translate-x-full"
-            }`}
+          className={`fixed top-0 bottom-0 left-0 z-40 flex w-64 flex-col border-r border-r-slate-200 bg-white transition-transform sm:translate-x-0 ${
+            isSideNavOpen ? "translate-x-0" : " -translate-x-full"
+          }`}
         >
           <SidebarHeader />
           <nav
@@ -26,7 +28,23 @@ export default function DefaultLayout({ component }) {
           >
             <div>
               <ul className="flex flex-1 flex-col gap-1 py-3">
-                {DataSidebar.map((item, idx) => <SidebarItem key={idx} item={item} setIsSideNavOpen={setIsSideNavOpen} />)}
+                {acc_type === "Admin" || acc_type === "Head Admin"
+                  ? AdminDataSidebar.map((item, idx) => (
+                      <SidebarItem
+                        key={idx}
+                        item={item}
+                        setIsSideNavOpen={setIsSideNavOpen}
+                        acc_type={acc_type}
+                      />
+                    ))
+                  : StaffDataSidebar.map((item, idx) => (
+                      <SidebarItem
+                        key={idx}
+                        item={item}
+                        setIsSideNavOpen={setIsSideNavOpen}
+                        acc_type={acc_type}
+                      />
+                    ))}
               </ul>
             </div>
           </nav>
@@ -35,21 +53,22 @@ export default function DefaultLayout({ component }) {
           <SidebarFooter />
         </aside>
         <div className="sm:ml-[256px] w-100">
-          <TopHeader isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen} />
-          <div className="p-4">
-            {component}
-          </div>
+          <TopHeader
+            isSideNavOpen={isSideNavOpen}
+            setIsSideNavOpen={setIsSideNavOpen}
+          />
+          <div className="p-4">{component}</div>
           <Footer />
         </div>
       </div>
 
-
       {/*  <!-- Backdrop --> */}
       <div
-        className={`fixed top-0 bottom-0 left-0 right-0 z-30 bg-slate-900/20 transition-colors sm:hidden ${isSideNavOpen ? "block" : "hidden"
-          }`}
+        className={`fixed top-0 bottom-0 left-0 right-0 z-30 bg-slate-900/20 transition-colors sm:hidden ${
+          isSideNavOpen ? "block" : "hidden"
+        }`}
         onClick={() => setIsSideNavOpen(false)}
       ></div>
     </>
-  )
+  );
 }
