8aeb4312cffffbfcf61c5389a55cb337ea607580
Rogu <116495454+rgrgogu@users.noreply.github.com>
2024-06-02 19:17:53 +0800

    adjusted

--- src/hooks/custom/useGetEvent.jsx
+++ src/hooks/custom/useGetEvent.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import { useEventContext } from "../context/useEventContext";
 import axios from 'axios';
+import { useAuthContext } from "../context/useAuthContext";
 
 
 const useGetEvent = () => {
     const [loading, setLoading] = useState(true);
+    const { credentials } = useAuthContext();
     const { dispatch } = useEventContext();
 
     useEffect(() => {
         const fetchEvents = async () => {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_API}/events/brgys_events/?brgy=BALITE&isArchived=false`);
-                console.log(`${import.meta.env.VITE_API}/events/brgys_events/`)
+                const response = await axios.get(`${import.meta.env.VITE_API}/events/brgys_events/?brgy=BALITE&isArchived=false`, {
+                    headers: { 'Authorization': `Bearer ${credentials.token}` },
+                });
+
                 dispatch({ type: "SET_EVENTS", payload: response.data })
             } catch (error) {
                 console.error('Error fetching events:', error);
